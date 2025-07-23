61699d595cbd51f6a42a7d7bcfcac61b5924d9f7
Hugo Saladini Da Silva <hg.saladini@gmail.com>
2025-07-23 18:51:48 -0300

    chore/norminette standard

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,13 +6,12 @@
 /*   By: hda-silv <hda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 16:52:07 by hda-silv          #+#    #+#             */
-/*   Updated: 2025/07/23 16:52:19 by hda-silv         ###   ########.fr       */
+/*   Updated: 2025/07/23 18:48:05 by hda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include "libft.h"
-
 
 void	ft_bzero(void *ptr, size_t n)
 {
