de4156ff0f91d794013a9e7749de1168eea45f46
Hugo Saladini Da Silva <hg.saladini@gmail.com>
2025-07-23 18:45:42 -0300

    ft_strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,25 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hda-silv <hda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/22 15:51:07 by hda-silv          #+#    #+#             */
-/*   Updated: 2025/07/23 18:44:17 by hda-silv         ###   ########.fr       */
+/*   Created: 2025/07/23 18:16:10 by hda-silv          #+#    #+#             */
+/*   Updated: 2025/07/23 18:43:53 by hda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+int	ft_strlen(char *str)
+{
+	int	i;
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isprint(int c);
-int		ft_isascii(int c);
-int		ft_strlen(char *str);
-void	ft_bzero(void *ptr, size_t n);
-void	*ft_memset(void *ptr, int c, size_t n);
-
-#endif
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
