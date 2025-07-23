c5d61c29d7cc333b8ad420a99c1b33a839d9db71
Hugo Saladini Da Silva <hg.saladini@gmail.com>
2025-07-23 16:53:22 -0300

    42

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,24 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hda-silv <hda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/22 15:51:07 by hda-silv          #+#    #+#             */
-/*   Updated: 2025/07/23 16:52:34 by hda-silv         ###   ########.fr       */
+/*   Created: 2025/07/23 16:52:07 by hda-silv          #+#    #+#             */
+/*   Updated: 2025/07/23 16:52:19 by hda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include <stddef.h>
+#include "libft.h"
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isprint(int c);
-int		ft_isascii(int c);
-void	ft_bzero(void *ptr, size_t n);
-void	*ft_memset(void *ptr, int c, size_t n);
 
-#endif
+void	ft_bzero(void *ptr, size_t n)
+{
+	ft_memset(ptr, 0, n);
+}
