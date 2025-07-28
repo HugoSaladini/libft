da6ff358b4fbba0b8d3d83b1f5eca993f2b85383
Hugo Saladini Da Silva <hg.saladini@gmail.com>
2025-07-28 16:41:00 -0300

    memcpy

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ NAME = libft.a
 CC = gcc
 FLAGS = -Wall -Wextra -Werror
 HEADER = libft.h
-SRCS = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_memset.c ft_bzero.c ft_isascii.c ft_isprint.c ft_memmove.c ft_strlen.c
+SRCS = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_memset.c ft_bzero.c ft_isascii.c ft_isprint.c 
+ft_memmove.c ft_strlen.c ft_memcpy.c 
 OBJS = $(SRCS:.c=.o)
 
 TEST = test
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,29 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hda-silv <hda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/22 15:51:07 by hda-silv          #+#    #+#             */
-/*   Updated: 2025/07/28 16:39:57 by hda-silv         ###   ########.fr       */
+/*   Created: 2025/07/28 16:16:40 by hda-silv          #+#    #+#             */
+/*   Updated: 2025/07/28 16:38:06 by hda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include <stddef.h>
 
-# include <stddef.h>
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t				i;
+	unsigned char		*d;
+	const unsigned char	*s;
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isprint(int c);
-int		ft_isascii(int c);
-int		ft_strlen(char *str);
-void	ft_bzero(void *ptr, size_t n);
-void	*ft_memset(void *ptr, int c, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
+	d = (unsigned char *) dest;
+	s = (const unsigned char *) src;
 
-#endif
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (d);
+}
