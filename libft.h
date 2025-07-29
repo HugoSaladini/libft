2eacd4ca4deaf2c905e474eb69db0a48c595366a
Hugo Saladini <hg.saladini@gmail.com>
2025-07-29 15:35:30 -0300

    strncmp

--- libft.h
+++ libft.h
@@ -24,6 +24,7 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_strlen(char *str);
 int		ft_memcmp(const void *src1, const void *src2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	ft_bzero(void *ptr, size_t n);
 void	*ft_memset(void *ptr, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
